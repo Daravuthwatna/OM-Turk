d73560b5b3da739fd999cffdfb32e699e31cf23e
Katasuki <daravuthwatna@gmail.com>
2023-10-03 08:40:49 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,416 @@
+var navbar = [
+  {
+    "navbar":"បុណ្យអុំទូក",
+    "href_menu":"",
+    "class-navbar":"navabr-1"
+  },
+  {
+    "navbar":"បណ្ដែតប្រទីប",
+    "href_menu":"",
+    "class-navbar":"navabr-2"
+  },
+  {
+    "navbar":"អកអំបុក",
+    "href_menu":"",
+    "class-navbar":"navabr-3"
+  },
+  {
+    "navbar":"សំពះព្រះខែ",
+    "href_menu":"",
+    "class-navbar":"navabr-4"
+  },
+]
+var left_home = [
+  {
+    "title-left":"ព្រះរាជពិធីបុណ្យអុំទូក បណ្ដែតប្រទីប​ អកអំបុក និង សំពះព្រះខែ",
+    "detail-left":"ព្រះរាជពិធីបុណ្យ អុំទូក បណ្តែតប្រទីប សំពះព្រះខែ និង អកអំបុក តែងតែប្រារព្ធក្នុងរយៈពេល៣ថ្ងៃ គឺពីថ្ងៃ១៤កើត ១៥កើត និងថ្ងៃ១រោច ខែកត្តិក នៅតាមទន្លេសាប។ ដើម្បីអបអរព្រះរាជពិធីបុណ្យនេះ បណ្តាញព័ត៌មានរបស់យើង សូមលើកយកនូវប្រវត្តិ ព្រះរាជពិធីបុណ្យអុំទូក បណ្តែតប្រទីប និងសំពះព្រះខែ អកអំបុក មកផ្សាយជូនប្រិយមិត្តបានស្វែងយល់។ ប្រវត្តិនៃព្រះរាជពិធីបុណ្យអុំទូក បណ្តែតប្រទីប និងសំពះព្រះខែ អកអំបុក ត្រូវបានដកស្រង់ចេញពី សៀវភៅ «ប្រវត្តិបុណ្យអុំទូក បណ្តែតប្រទីប និងសំពះព្រះខែ អកអំបុក» របស់គណៈកម្មាធិការ រៀបចំបុណ្យជាតិ អន្តរជាតិ។",
+    "button-left":"អាន​បន្ត",
+  }
+]
+var right_home = [
+  {"right-img":"img/1.jpg","right-class":"img-1"},
+  {"right-img":"img/2.jpg","right-class":"img-2"},
+  {"right-img":"img/3.jpg","right-class":"img-3"},
+  {"right-img":"img/4.jpg","right-class":"img-4"},
+]
+var detail_item = [
+  {"profile-detail":"img/profile(1).jpg",},
+  {"profile-detail":"img/profile(2).jpg",},
+  {"profile-detail":"img/profile(3).jpg",},
+  {"profile-detail":"img/profile(4).jpg",},
+  {"profile-detail":"img/profile(5).jpg",},
+]
+var detail_menu = [
+  {"menu-drop":"ឈ្មោះ​ផ្លូវការណ៍","sub-menu-drop":[
+    {"detail":"ព្រះរាជពិធីបុណ្យអុំទូក បណ្ដែតប្រទីប​  និង អកអំបុក សំពះព្រះខែ"}
+  ]},
+  {"menu-drop":"ប្រភេទបុណ្យ","sub-menu-drop":[
+    {"detail":"បុណ្យសន្តៈរដូវ (រដូវរំហើយ)"}
+  ]},
+  {"menu-drop":"ប្រារព្ធដោយ","sub-menu-drop":[
+    {"detail":"ជនជាតិខ្មែរ"}
+  ]},
+  {"menu-drop":"ថ្ងៃចាប់ផ្ដើម និង ថ្ងៃបញ្ចប់","sub-menu-drop":[
+    {"detail":"រៀងរាល់ខែវិច្ឆិកា"}
+  ]},
+  {"menu-drop":"ប្រវត្តិទេវៈកថា","sub-menu-drop":[
+    {"detail":"ទាក់ទងនិងសាសនា ហិណ្ឌូ និង ពុទ្ធសាសនា"}
+  ]},
+  {"menu-drop":"ប្រារព្ធលើកដំបូង","sub-menu-drop":[
+    {"detail":"រាជព្រះបាទ នរោត្ដម"}
+  ]},
+  {"menu-drop":"ចុះកាលបរិច្ឆេទ","sub-menu-drop":[
+    {"detail":"ឆ្នាំ ១៨៧៣"}
+  ]},
+]
+var paragraph = [
+  {
+    "left-slide":"img/left(1).jpg",
+    "title-right-slide":"បុណ្យអុំទូក",
+    "class-title-right":"title-right-1",
+    "paragraph-right-slide":"&nbsp;បុណ្យអុំទូក ត្រូវបានគេតំណាលថា ជាពិធីបុណ្យរំលឹកដល់ការដឹងគុណ ចំពោះកងទ័ពជើងទឹករបស់កម្ពុជា ដែលបានពលីជីវិតបូជាចំពោះទឹកដីក្នុងចម្បាំងខ្មែរ-ចាម នាស.វទី​១២ ក្នុងរាជ ព្រះបាទ ​ជ័យវរ្ម័នទី៧ ក្នុងរវាងឆ្នាំ ១១៧៧ ដល់ឆ្នាំ ១១៨១ នៃគ.សករាជ ។ រីរៈភាពនៃកងទ័ពជើងទឹករបស់កម្ពុជានេះហើយ ដែលត្រូវបានគេជឿថា ព្រះបាទជ័យវរ្ម័នទី៧ បានប្រារព្ធពិធី ប្រណាំងទូកនេះឡើង ដែលមានកងទ័ពទូកជាច្រើនក្រាសក្រែល ចូលរួម នេះបើយោងតាមចម្លាក់នា ប្រាសាទបាយ័ន និង ប្រាសាទបន្ទាយឆ្មារ ដែលពណនាអំពី​ព្រឹត្តិការណ៍​នៃចម្បាំងនេះ ។ ពិធីប្រណាំងទូក នេះទៀតសោតមានកត់ត្រាក្នុងឯកសារមហាបុរសខ្មែរ ឆ្នាំ ១៩២៨ ផែនដីព្រះស្រីជេដ្ឋា និង ផែនដីព្រះចន្ទរាជា នាស.វទី១៦ ។​"
+  },
+
+  {
+    "left-slide":"img/left(2).jpg",
+    "title-right-slide":"បណ្ដែតប្រទីប",
+    "class-title-right":"title-right-2",
+    "paragraph-right-slide":"&nbsp;ពិធីបណ្ដែតប្រទីប ឬ បណ្ដែតគោម ជាកិច្ចពិធីមួយដែលមានក្នុងឯកសារមហាបុរសខ្មែរ ឆ្នាំ ១៩២៨ ផែនដី ព្រះចន្ទរាជា គ.សករាជ ១៥២២ ក្រោយព្រះអង្គឈ្នះសង្គ្រាមស្ដេចកន នាបន្ទាយចតុមុខ ព្រះអង្គបានប្រារព្ធពិធីនេះឡើង ដើម្បីដឹងគុណដល់ព្រះម៉ែគង្គា ម្ដាយនៃការបង្កើតប្រភពទឹក ដើម្បីជាកិច្ចសុំខមាលទោសផង និង បន់ស្រន់សូមកុំឱ្យមានទឹកជំនន់នៅតំបន់នេះទៀត ។'ព្រះម៉ែគង្គា' ដែលមានព្រះហស្ថ (ដៃ) កាន់ផ្កាឈូក និង ក្អមទឹក មានយាន្តជំនិះ ជាសត្វទឹក មកករ ត្រីរាជ ក្រពើ អណ្តើក .ល។ គង្គា មកពីពាក្យ ទន្លេនៅឥណ្ឌាឈ្មោះថា កាំងកា ជាទន្លេ ពិសិដ្ឋ នៃ គ្រប់ជំនឿសាសនាទាំងអស់នៅ ឥណ្ឌា ជាពិសេស ពួកហិណ្ឌូ ទោះបី ពុំមានទន្លេ ។ "
+  },
+
+  {
+    "left-slide":"img/left(3).jpg",
+    "title-right-slide":"អកអំបុក",
+    "class-title-right":"title-right-3",
+    "paragraph-right-slide":"&nbsp;ប្រវត្តិពិធី អកអំបុក ប្រវត្តិនេះមានមកយូលង់ណាស់មកហើយ ដែលគេជឿថា ក្នុងរាជ ព្រះបរមរាជាទី២ ក្នុងឆ្នាំ (១៥៦៨ នៃ គ.ស) ដែលកើតឡើងតាមរយៈព្រះសុបិន របស់ព្រះអង្គ ដែលព្រះអង្គ សុបិន ឃើញព្រះឥន្ទ្រ ប្រយុទ្ធគ្នាជាមួយពួកបីសាច នៅក្នុងបន្ទាយលង្វែក ហើយទ្រង់ឃើញ ព្រះឥន្ទ្រ បានចោលកាំរស្មីរន្ទះសម្លាប់ពួកបីសាចនោះ ភ្លាមនោះសម្លេងរន្ទះ បានបាញ់បន្លឺឡើង ធ្វើឲ្យទ្រង់ភ្ញាក់ឡើង លុះព្រឹកឡើងទ្រង់ចាត់ឲ្យមន្ត្រីហែរហមទៅមើលដីបរិវេណជុំវិញក្នុងបន្ទាយលង្វែក ក៏ប្រទះឃើញដីមួយកន្លែងមានស្នាមរន្ទះបាញ់ពិតមែននោះ ព្រះរាជាទ្រង់ ក៏បាននាំព្រះមហេសី ស្រីស្នំ ក្រុមការ រួមទាំងញាតិវង្ស ព្រាហ្មបរោហិត មន្រ្តីសេនាបតី ទាំងអស់ ធ្វើពិធីសែនក្រុងពាលី បួងសួងដល់ទេវតាទាំង ៨ទិស អស់រយៈពេល ៣ថ្ងៃ ។"
+  },
+
+  {
+    "left-slide":"img/left(4).jpg",
+    "title-right-slide":"សំពះព្រះខែ",
+    "class-title-right":"title-right-4",
+    "paragraph-right-slide":"&nbsp;ក្នុងដើមភទ្ទកប្បនេះព្រះបរមពោធិសត្វនៃយើង ទ្រង់យោងយកកំណើតជា សសបណ្ឌិត កំណើតជាទន្សាយ ថ្ងៃមួយខែពេញបូរមី សសបណ្ឌិត បានអធិដ្ឋានឧបោសថសីលហើយឧទ្ទិសសមំសះជាទានបរមត្តបារមីដើម្បីបំពេញ ពោធិសម្ភារឱ្យបានត្រាស់ជាព្រះពុទ្ធ ។ ព្រះឥន្ទបានទ្រង់ជ្រាបពីហេតុការណ៍នោះក៏ទ្រង់ក្រឡាព្រះអង្គជាព្រាហ្មចាស់ មកសុំ មំសា សសបណ្ឌិត ជាអាហារ ។ សសបណ្ឌិតពោធិសត្វបានឱ្យព្រាហ្មចាស់នោះបង្កាត់ភ្លើងភ្នក់ ហើយក្នុង ពេលដែលភ្លើងកំពុងឆេះ សន្ធោសន្ធៅ សសបណ្ឌិត ក៏រលាស់ខ្លួនបីដងដើម្បីឱ្យសត្វល្អិតដែលជាប់នឹងរោមជ្រុះចេញ រួចក៏លោតចូលទៅក្នុងភ្នក់ភ្លើងភ្លាម ។"
+  },
+]
+
+var left_paragraph = [
+  {"left-paragraph-img":"img/more-left(1).jpg","sub-left-paragraph":[
+    {"left-paragraph-text":"នារីរាំនៅលើក្បាលទូក ដែលមានក្នុងឯកសារមហាបុរសខ្មែរ ឆ្នាំ ១៩២៨ ផែនដី ព្រះស្រីជេដ្ឋា​ ឬ ហ្លួងព្រះស្ដេចកន គ.សរាជ ១៥១៦ ព្រះស្ដេចកនតាំងពិធីប្រណាំងទូក មាននារីរាំក្បាលទូកបង្អួតកងទ័ពជើងទឹក ទៅកាន់ ព្រះចន្ទរាជា នារាជធានី​ទួលបាសាន ខេត្តកំពង់ចាម​ ។","link-left-paragraph":"BB"}
+  ]
+  },
+  {"left-paragraph-img":"img/more-left(2).jpg","sub-left-paragraph":[
+    {"left-paragraph-text":"ពិធីកាត់ព្រ័ត្យ ប្រារព្ធឡើងនាថ្ងៃចុងក្រោយ នៃពិធីបុណ្យអុំទូក ជាពេលដែលទូកប្រណាំងទាំងអស់បានស្វែងរកឃើញម្ចាស់ជ័យលាភីលេខ ១ ដែលត្រូវបានអង្គព្រះមហាក្សត្រនៃកម្ពុជា ប្រសិទ្ធី​នាមថាជា 'ស្ដេចទូក' ។","link-left-paragraph":"BB"}
+  ]
+  },
+  {"left-paragraph-img":"img/more-left(3).jpg","sub-left-paragraph":[
+    {"left-paragraph-text":"ពិធីបុណ្យអុំទូករបស់កម្ពុជាត្រូវបានកត់​ ជាលើកដំបូងក្នុងរាជព្រះបាទ នរោត្ដម ពីសំណាក់ក្រុមវិទូបារាំង ក្នុងឆ្នាំ ១៨៧៣ ។​ នៅដើមឆ្នាំ ១៨៨៧ រូបភាពនៃទូកប្រណាំងរបស់កម្ពុជាបានបង្ហាញខ្លួននៅក្នុងការបោះពុម្ពផ្សាយរបស់បារាំងជាបន្តបន្ទាប់។","link-left-paragraph":"BB"}
+  ]
+  },
+]
+
+var footer = [
+  {
+    "tltle-footer":"ព័ត៌មានលម្អិត",
+    "sub-title-footer":[
+      {"menu-footer":"បុណ្យអុំទូក","link":""},
+      {"menu-footer":"បណ្ដែតប្រទីប","link":""},
+      {"menu-footer":"អកអំបុក","link":""},
+      {"menu-footer":"សំពះព្រះខែ","link":""},
+    ],
+  },
+  {
+    "tltle-footer":"ព័ត៌មានផ្សេងៗ",
+    "sub-title-footer":[
+      {"menu-footer":"នារីរាំក្បាលទូក","link-menu-footer":""},
+      {"menu-footer":"ពិធីកាត់ព្រ័ត្យ","link-menu-footer":""},
+      {"menu-footer":"ក្រុមវិទូបារាំងកត់ត្រា","link-menu-footer":""},
+    ],
+  },
+  {
+    "tltle-footer":"ឯកសារយោង",
+    "sub-title-footer":[
+      {"menu-footer":"ព័ត៌មានផ្ទាល់ខ្លួន","link":""},
+    ],
+  }
+]
+var icon =[
+  {"footer-icon":"fa-brands fa-facebook"},
+  {"footer-icon":"fa-brands fa-github"},
+  {"footer-icon":"fa-brands fa-instagram"},
+  {"footer-icon":"fa-brands fa-discord"},
+]
+
+$(document).ready(function(){
+  $(".scrolltop i").on("click",function(){
+    $("html, body").animate({
+      scrollTop: $(".menu").offset().top
+    }, 5000)
+  })
+
+  GetNavbar()
+  function GetNavbar(){
+    var txt_navbar = ""
+    navbar.forEach((e)=>{
+      txt_navbar += `
+      <li class="${e["class-navbar"]}">
+        <a>${e["navbar"]}</a>
+      </li>`
+    })
+    $(".menu").find("ul").append(txt_navbar)
+    Getscroll()
+    function Getscroll(){
+      $(".navabr-1").on("click",function(){
+        $('html, body').animate({
+          scrollTop: $(".title-right-1").offset().top
+        }, 1500)
+      })
+      $(".navabr-2").on("click",function(){
+        $('html, body').animate({
+          scrollTop: $(".title-right-2").offset().top
+        }, 2500)
+      })
+      $(".navabr-3").on("click",function(){
+        $('html, body').animate({
+          scrollTop: $(".title-right-3").offset().top
+        }, 3500)
+      })
+      $(".navabr-4").on("click",function(){
+        $('html, body').animate({
+          scrollTop: $(".title-right-4").offset().top
+        }, 4500)
+      })
+    }
+  }
+  GetLeftHome()
+  function GetLeftHome(){
+    var txt_left_home = ""
+    left_home.forEach((e)=>{
+      txt_left_home += `
+      <h1>${e["title-left"]}</h1>
+      <p>${e["detail-left"]}</p>
+      <button>${e["button-left"]}</button>`
+    })
+    $(".left-home").find(".box").append(txt_left_home)
+  }
+  $(".left-home button").on("click",function(){
+    $("html, body").animate({
+      scrollTop: $(".stop").offset().top
+    }, 1000)
+  })
+  GetRightHome()
+  function GetRightHome(){
+    var txt_right_home = ""
+    right_home.forEach((e)=>{
+      txt_right_home += `
+      <li>
+        <img src="${e["right-img"]}" alt="" class="${e["right-class"]} item-img">
+      </li>`
+    })
+    $(".right-home").find("ul").append(txt_right_home)
+  }
+  $(".home-page").on("click","li",function(){
+    var ethis = $(this)
+    var img = ethis.find("img").attr("src")
+    var txtpopup = `
+    <div class="popup">
+      <div class="frm-view">
+        <div class="left-popup">
+          <img src="${img}" alt="">
+          <i class="fa-solid fa-circle-xmark btn-close"></i>
+        </div>
+      </div>
+    </div>`
+    $("body").append(txtpopup)
+  })
+  GetDetailMenu()
+  function GetDetailMenu(){
+    var txt_detail_menu = ""
+    detail_menu.forEach((e)=>{
+      txt_detail_menu += `
+      <li class="drop">
+        <h4>${e["menu-drop"]}<i class="fa-solid fa-plus"></i></h4>
+      </li>`
+    })
+    $(".detail-menu").find(".box ul").append(txt_detail_menu)
+  }
+  $(".detail-menu").on("click",".box ul .drop",function(){
+    var ithis = $(this)
+    var i = ithis.index()
+    var sub_menu_detail = detail_menu[i]["sub-menu-drop"]
+    var txt_sub_menu = ""
+    sub_menu_detail.forEach((e)=>{
+      txt_sub_menu += `
+      <li>
+        <p>${e["detail"]}</p>
+      </li>`
+    })
+    var sub_menu = `<ul class="detail-menu-drop">${txt_sub_menu}</ul>`
+    if(ithis.find(".detail-menu-drop").length == 0){
+      ithis.append(sub_menu)
+      ithis.find("i").removeClass("fa-plus")
+      ithis.find("i").addClass("fa-minus")
+    }
+    else{
+      if(ithis.find(".fa-minus").length > 0){
+        ithis.find(".detail-menu-drop").slideUp()
+        ithis.find("i").addClass("fa-plus")
+        ithis.find("i").removeClass("fa-minus")
+      }
+      else{
+        ithis.find(".detail-menu-drop").slideDown()
+        ithis.find("i").addClass("fa-minus")
+        ithis.find("i").removeClass("fa-plus")
+      }
+    }
+  })
+  GetDetailItem()
+  var slide_item = $("body").find(".detail-item")
+  var numslide_item = slide_item.length
+
+  var ind_sliide_item = 0
+  slide_item.hide()
+  slide_item.eq(ind_sliide_item).show()
+
+  var my_slide_item = setInterval(function(){
+    nextslide()
+  },2000)
+  $(".detail-item-container").on("mouseover",function(){
+    clearInterval(my_slide_item)
+  })
+  $(".detail-item-container").on("mouseout",function(){
+    my_slide_item = setInterval(function(){
+      nextslide()
+    },2000)
+  })
+
+  function nextslide(){
+    slide_item.eq(ind_sliide_item).hide()
+    ind_sliide_item++
+    if(ind_sliide_item==numslide_item){
+      ind_sliide_item=0
+    }
+    slide_item.eq(ind_sliide_item).show()
+  }
+  $(".btn-next").on("click",function(){
+    nextslide()
+  })
+  $(".btn-back").on("click",function(){
+    slide_item.eq(ind_sliide_item).hide()
+    ind_sliide_item--
+    if(ind_sliide_item<0){
+      ind_sliide_item = numslide_item -1
+    }
+    slide_item.eq(ind_sliide_item).show()
+  })
+  function GetDetailItem(){
+    var txt_detail_item = ""
+    detail_item.forEach((e)=>{
+      txt_detail_item += `
+        <div class="detail-item">
+          <div class="detail-item-left">
+            <img src="${e["profile-detail"]}" alt="">
+          </div>
+        </div>`
+    })
+    $(".detail-item-container").find(".box-item").append(txt_detail_item)
+  }
+  GetParagraph()
+  function GetParagraph(){
+    var txt_paragraph = ""
+    paragraph.forEach((e)=>{
+      txt_paragraph += `
+      <div class="${e["class-title-right"]}"></div>
+      <div class="col-lg-6 col-xl-6 left-paragraph">
+        <div class="box-left-paragraph">
+          <img src="${e["left-slide"]}" alt="">
+        </div>
+      </div>
+      <div class="col-lg-6 col-xl-6 right-paragraph">
+        <div class="box-right-paragraph">
+          <h3>${e["title-right-slide"]}</h3>
+          <p>${e["paragraph-right-slide"]}</p>
+        </div>
+      </div>`
+    })
+    $(".paragraph-container").find(".box-paragraph").append(txt_paragraph)
+  }
+  $(".paragraph-container").on("mouseover",".right-paragraph .box-right-paragraph h3",function(){
+    $(this).css("color","#b38b59")
+    $(this).css("transition","1s")
+  })
+  $(".paragraph-container").on("mouseout",".right-paragraph .box-right-paragraph h3",function(){
+    $(this).css("color","#efefefe")
+    $(this).css("transition","1s")
+  })
+  $(".paragraph-container").on("click",".left-paragraph",function(){
+    var ethis = $(this)
+    var img = ethis.find("img").attr("src")
+    var txtpopup = `
+    <div class="popup">
+      <div class="frm-view">
+        <div class="left-popup">
+            <img src="${img}" alt="">
+            <i class="fa-solid fa-circle-xmark btn-close"></i>
+        </div>
+      </div>
+    </div>`
+    $("body").append(txtpopup)
+  })
+  $("body").on("click",".popup .btn-close",function(){
+    $(this).parents(".popup").remove()
+  })
+
+  GetParagraphLeft()
+  function GetParagraphLeft(){
+    var txt_left_paragraph = ""
+    left_paragraph.forEach((e)=>{
+      var sub_left_paragraph = e["sub-left-paragraph"]
+      var txt_sub_left_paragraph = ""
+      sub_left_paragraph.forEach((e1)=>{
+        txt_sub_left_paragraph += `
+        <li>
+          <p>${e1["left-paragraph-text"]}</p>
+        </li>`
+      })
+      txt_left_paragraph += `
+      <div class="box-more-left-paragraph">
+        <img src="${e["left-paragraph-img"]}" alt="">
+        <ul>${txt_sub_left_paragraph}</ul>
+      </div>`
+    })
+    $(".more-left-paragraph").find(".row").append(txt_left_paragraph)
+  }
+
+  GetFooter()
+  function GetFooter(){
+    var txt_footer = ""
+    footer.forEach((e)=>{
+      var sub_footer = e["sub-title-footer"]
+      var txt_sub_fotter = ""
+      sub_footer.forEach((e1)=>{
+        txt_sub_fotter += `
+        <li>
+          <a href="${e1["link"]}">${e1["menu-footer"]}</a>
+        </li>`
+      })
+      txt_footer += `
+      <li>
+        <p>${e["tltle-footer"]}</p>
+        <ul>${txt_sub_fotter}</ul>
+      </li>`
+    })
+    $(".box-footer").find("ul").append(txt_footer)
+  }
+
+  GetIcon()
+  function GetIcon(){
+    var txt_icon = ""
+    icon.forEach((e)=>{
+      txt_icon += `
+      <li>
+        <i class="${e["footer-icon"]}"></i>
+      </li>`
+    })
+    $(".box-icon").find("ul").append(txt_icon)
+  }
+})
